1a6fced2445c8e70e73cf24a2e6beb82438927ff
Angel Delgado <scrapmechanickid@gmail.com>
2024-10-29 17:03:39 -0400

    Add Selection::remove

--- crates/hydrogen_data_structures/src/selection.rs
+++ crates/hydrogen_data_structures/src/selection.rs
@@ -26,14 +26,25 @@ impl<T> Selection<T> {
         }
     }
 
+    pub fn get_values(&self) -> &Vec<T> {
+        match self {
+            Selection::Whitelist(values) => values,
+            Selection::Blacklist(values) => values,
+        }
+    }
+
+    pub fn get_values_mut(&mut self) -> &mut Vec<T> {
+        match self {
+            Selection::Whitelist(values) => values,
+            Selection::Blacklist(values) => values,
+        }
+    }
+
     pub fn insert(&mut self, value: T) -> bool
     where
         T: PartialEq,
     {
-        let values = match self {
-            Selection::Whitelist(values) => values,
-            Selection::Blacklist(values) => values,
-        };
+        let values = self.get_values_mut();
 
         if !values.contains(&value) {
             values.push(value);
@@ -41,5 +52,22 @@ impl<T> Selection<T> {
         } else {
             false
         }
+    }
+
+    pub fn remove(&mut self, value: &T) -> Option<T>
+    where
+        T: PartialEq,
+    {
+        let values = self.get_values_mut();
+
+        let mut found_index: Option<usize> = None;
+        for (i, other_value) in values.iter().enumerate() {
+            if other_value == value {
+                found_index = Some(i);
+                break;
+            }
+        }
+
+        found_index.map(|i| values.swap_remove(i))
     }
 }
--- crates/hydrogen_net/src/ecs_net.rs
+++ crates/hydrogen_net/src/ecs_net.rs
@@ -1,3 +1,4 @@
+use hydrogen_data_structures::selection::Selection;
 use hydrogen_ecs::component::Component;
 use serde::{Deserialize, Serialize};
 
@@ -6,6 +7,7 @@ use crate::{comm::NetMessage, server_client::ClientId};
 #[derive(Debug, Component)]
 pub struct Replicated {
     pub owner: Option<ClientId>,
+    pub replicate_to: Selection<ClientId>,
 }
 
 #[derive(Debug, Serialize, Deserialize, NetMessage)]
