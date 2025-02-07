017d437e2d558ce0073fae7b3c689aa4f27d1f5f
Angel Delgado <scrapmechanickid@gmail.com>
2025-02-06 19:16:51 -0500

    automatically implement dependency provider traits for references

--- crates/hydrogen_core/src/dependency.rs
+++ crates/hydrogen_core/src/dependency.rs
@@ -7,3 +7,30 @@ pub trait Dependency<D> {
 pub trait DependencyMut<D> {
     fn dep_mut(&mut self) -> &mut D;
 }
+
+impl<T, D> Dependency<D> for &T
+where
+    T: Dependency<D>,
+{
+    fn dep(&self) -> &D {
+        (*self).dep()
+    }
+}
+
+impl<T, D> Dependency<D> for &mut T
+where
+    T: Dependency<D>,
+{
+    fn dep(&self) -> &D {
+        (**self).dep()
+    }
+}
+
+impl<T, D> DependencyMut<D> for &mut T
+where
+    T: DependencyMut<D>,
+{
+    fn dep_mut(&mut self) -> &mut D {
+        (*self).dep_mut()
+    }
+}
